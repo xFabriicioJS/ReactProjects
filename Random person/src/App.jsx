3b72747ec16e3c9ff0500deb8ab3fb4ddc9c78f4
xFabriicioJS <fabriciocostamonteiro@hotmail.com>
2022-06-10 15:49:16 -0300

    finalização randomPerson

--- Random person/src/App.jsx
+++ Random person/src/App.jsx
@@ -50,7 +50,11 @@ function App() {
   },[])
 
   const handleValue = (e) => {
-    console.log(e.target)
+    if(e.target.classList.contains('icon')){
+      const newValue = e.target.dataset.label;
+      setTitle(newValue)
+      setValue(person[newValue]);
+    }
   }
 
   return <main>
